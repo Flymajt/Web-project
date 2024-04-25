ab7bcf6643e1027591c2896f1e1fac227d262062
Flymajt <jaraxxus@volny.cz>
2024-04-25 15:50:19 +0200

    Přidání python souboru i json souboru + funkce pro zápis do jsonu

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, redirect, session, url_for, make_response
+import os
+import json
+
+app = Flask (__name__)
+app.config["SECRET_KEY"] = "123456789"
+
+
+def zapis_do_json(nazev_souboru, data_na_zapis):
+    aktivni_soubor = os.path.dirname(__file__)
+    SITE_ROOT = os.path.realpath(aktivni_soubor)
+    json_url = os.path.join(SITE_ROOT, "static/data", f"{nazev_souboru}.json")
+    UZIVATELE = json.load(open(json_url,"r",encoding="utf-8"))
+    UZIVATELE.append(data_na_zapis)
+    with open(json_url, "w", encoding="utf-8") as outline:
+        json.dump(UZIVATELE, outline)
+
+    return
