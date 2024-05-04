83291fc3e815ccee9467e427bcf819f1102c48b9
Gotyx <mrgotyx@gmail.com>
2024-05-04 17:59:41 +0200

    Přidávání hudby - Povolení pouze .mp3
    pokud uživatel přidá něco co nekončí .mp3 tak se to nepřidá

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, session, url_for, make_response
+from flask import Flask, flash, request, render_template, redirect, session, url_for, make_response
 import os
 import json
 
@@ -7,10 +7,6 @@ UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static/data/songs")
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "123456789"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
-def slozka_existuje(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
 
 def precti_json(nazev_souboru):
     aktivni_soubor = os.path.dirname(__file__)
@@ -137,25 +133,26 @@ def zpracuj_song():
     author = request.form.get("author")
     album = request.form.get("album")
 
-    slozka_existuje(app.config["UPLOAD_FOLDER"])
-
     songs = precti_json_songs("songs")
     for u in songs:
         if u ["title"] == title:
             return redirect(url_for("index"))
 
     songfile = request.files["songfile"]
-    songfile.save(os.path.join(app.config["UPLOAD_FOLDER"], songfile.filename))
-
-    new_song = {
+    if songfile.filename.endswith(".mp3"):
+        songfile.save(os.path.join(app.config["UPLOAD_FOLDER"], songfile.filename))
+        
+        new_song = {
         "title": title,
         "author": author,
         "album": album,
         "songfile": songfile.filename
     }
-    zapis_do_json_songs("songs", new_song)
+        zapis_do_json_songs("songs", new_song)
 
-    return redirect(url_for("index"))
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("add_song"))
 
 if __name__ == "__main__":
     app.run(debug=True)
