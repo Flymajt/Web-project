923946b88dec08e1b3ad4f1728f086fb8a0fe957
Gotyx <mrgotyx@gmail.com>
2024-05-04 18:13:31 +0200

    Přidávání hudby - Alba - start
    toto je menší dlc kategorie, na add-song člověk může přidat i album, pak plánuji aby si člověk vybral z listu alb při přidávání písničky místo toho aby to manuálně zadával

--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, flash, request, render_template, redirect, session, url_for, make_response
+from flask import Flask, request, render_template, redirect, session, url_for, make_response
 import os
 import json
 
-UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static/data/songs")
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static/data")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "123456789"
@@ -39,6 +39,23 @@ def zapis_do_json_songs(nazev_souboru, data_na_zapis):
     SONGS.append(data_na_zapis)
     with open(json_url, "w", encoding="utf-8") as outline:
         json.dump(SONGS, outline)
+
+    return
+
+def precti_json_albums(nazev_souboru):
+    aktivni_soubor = os.path.dirname(__file__)
+    SITE_ROOT = os.path.realpath(aktivni_soubor)
+    json_url = os.path.join(SITE_ROOT, "static/data", f"{nazev_souboru}.json")
+    ALBUMS = json.load(open(json_url,"r",encoding="utf-8"))
+    return ALBUMS
+def zapis_do_json_albums(nazev_souboru, data_na_zapis):
+    aktivni_soubor = os.path.dirname(__file__)
+    SITE_ROOT = os.path.realpath(aktivni_soubor)
+    json_url = os.path.join(SITE_ROOT, "static/data", f"{nazev_souboru}.json")
+    ALBUMS = json.load(open(json_url,"r",encoding="utf-8"))
+    ALBUMS.append(data_na_zapis)
+    with open(json_url, "w", encoding="utf-8") as outline:
+        json.dump(ALBUMS, outline)
 
     return
 
@@ -140,7 +157,7 @@ def zpracuj_song():
 
     songfile = request.files["songfile"]
     if songfile.filename.endswith(".mp3"):
-        songfile.save(os.path.join(app.config["UPLOAD_FOLDER"], songfile.filename))
+        songfile.save(os.path.join(app.config["UPLOAD_FOLDER" + "/songs"], songfile.filename))
         
         new_song = {
         "title": title,
@@ -154,6 +171,33 @@ def zpracuj_song():
     else:
         return redirect(url_for("add_song"))
 
+@app.route('/zpracuj-album', methods=["POST"])
+def zpracuj_album():
+    title = request.form.get("title")
+    author = request.form.get("author")
+    release = request.form.get("release")
+
+    albums = precti_json_albums("albums")
+    for u in albums:
+        if u ["title"] == title:
+            return redirect(url_for("index"))
+
+    albumfile = request.files["albumfile"]
+    if albumfile.filename.endswith(".png" or ".jpg" or ".jpeg"):
+        albumfile.save(os.path.join(app.config["UPLOAD_FOLDER" + "/albums"], albumfile.filename))
+        
+        new_album = {
+        "title": title,
+        "author": author,
+        "release": release,
+        "albumfile": albumfile.filename
+    }
+        zapis_do_json_albums("albums", new_album)
+
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("add_song"))
+    
 if __name__ == "__main__":
     app.run(debug=True)
 
